cb88200dcd14c78bcd66c8f3b01d1fc955c6070b
Chris M. Hiatt <chris.hiatt@protonmail.com>
2023-02-18 15:35:12 +0100

    Param/Admin works

--- tests/emulator.test.ts
+++ tests/emulator.test.ts
@@ -11,144 +11,144 @@ import { KeyHash } from "../src/types/general/derived/hash/keyHash.ts";
 import { Data } from "../src/types/general/fundamental/type.ts";
 import { genPositive, randomChoice } from "../src/utils/generators.ts";
 
-// Deno.test("emulator", async () => {
-//   const allUsers = await User.genSeveral(10n, 2n); // TODO more
-//   const accounts = allUsers.map((u) => u.account);
-//   console.log(`accounts: ${accounts}`);
-//   const emulator = new Lucid.Emulator(accounts);
-//   const traces: string[] = [];
-//   const actionCounts = new Map<string, number>();
-//   const iterations = 100; // TODO more
-//   for (let i = 0; i < iterations; i++) {
-//     console.log(`\niteration: ${i} - block: ${emulator.blockHeight}`);
-//     const lucid = await Lucid.Lucid.new(emulator);
-//     const user = await User.from(lucid, randomChoice(allUsers).privateKey);
-
-//     // TODO multiple parallel users and actions (requires logging of spent contract utxos or error handling)
-//     // const users = await Promise.all(
-//     //   randomSubset(allUsers).map(async (user) => {
-//     //     const lucid = await Lucid.Lucid.new(emulator);
-//     //     return User.from(lucid, user.privateKey);
-//     //   }),
-//     // );
-//     // console.log(`users: ${users.length}`);
-//     try {
-//       // for (const user of users) {
-//       const hashes = await user
-//         .generateActions()
-//         .then((actions) =>
-//           Promise.all(
-//             actions.map(async (action) => {
-//               const type = action.type;
-//               actionCounts.set(type, (actionCounts.get(type) ?? 0) + 1);
-//               const tx = action.tx(user.lucid.newTx());
-//               // console.log(tx);
-//               return await tx
-//                 .complete()
-//                 .then((completed) => {
-//                   // console.log(completed.txComplete.to_js_value());
-//                   return completed
-//                     .sign()
-//                     .complete()
-//                     .then((signed) => {
-//                       // console.log(signed.txSigned.to_js_value());
-//                       return signed.submit();
-//                     });
-//                 });
-//             }),
-//           )
-//         );
-//       // console.log(hashes);
-//       traces.push(...hashes);
-//       // }
-//     } catch (e) {
-//       throw new Error(`Error: ${e}`);
-//     }
-//     emulator.awaitBlock(Number(genPositive()));
-//   }
-//   console.log(`traces.length: ${traces.length}`);
-//   for (const [type, count] of actionCounts) {
-//     console.log(`${type}: ${count}`);
-//   }
-//   // for (const user of allUsers) {
-//   //   const lucid = await Lucid.Lucid.new(emulator);
-//   //   const user_ = await User.from(lucid, user.privateKey);
-//   //   console.log(await user_.lucid.wallet.getUtxos());
-//   // }
-// });
-
-Deno.test("constr", async () => {
-  for (let i = 0; i < 100; i++) {
-    const privateKey = Lucid.generatePrivateKey();
-
-    const address = await (await Lucid.Lucid.new(undefined, "Custom"))
-      .selectWalletFromPrivateKey(privateKey).wallet.address();
-
-    const emulator = new Lucid.Emulator([{
-      address,
-      assets: { lovelace: 3000000000n },
-    }]);
-
+Deno.test("emulator", async () => {
+  const allUsers = await User.genSeveral(10n, 2n); // TODO more
+  const accounts = allUsers.map((u) => u.account);
+  console.log(`accounts: ${accounts}`);
+  const emulator = new Lucid.Emulator(accounts);
+  const traces: string[] = [];
+  const actionCounts = new Map<string, number>();
+  const iterations = 100; // TODO more
+  for (let i = 0; i < iterations; i++) {
+    console.log(`\niteration: ${i} - block: ${emulator.blockHeight}`);
     const lucid = await Lucid.Lucid.new(emulator);
+    const user = await User.from(lucid, randomChoice(allUsers).privateKey);
 
-    lucid.selectWalletFromPrivateKey(privateKey);
-
-    const contract = new Contract(lucid);
-    const param = PParam.ptype.genData();
-    const param_ = new Param(
-      KeyHash.fromCredential(
-        lucid.utils.getAddressDetails(address).paymentCredential!,
-      ),
-      param.virtual,
-      param.weights,
-      param.jumpSizes,
-    );
-    const paramDatum = new ParamDatum(param_);
-    const peuclidDatum = PPreEuclidDatum.genPType(); //only need this for ParamDatum, so this is fine
-
-    const datum = peuclidDatum.pconstant(paramDatum);
-
-    console.log(datum);
-    (datum.fields[0] as Lucid.Constr<Data>).fields.forEach((f) => {
-      console.log(f);
-      console.log("---");
-    });
+    // TODO multiple parallel users and actions (requires logging of spent contract utxos or error handling)
+    // const users = await Promise.all(
+    //   randomSubset(allUsers).map(async (user) => {
+    //     const lucid = await Lucid.Lucid.new(emulator);
+    //     return User.from(lucid, user.privateKey);
+    //   }),
+    // );
+    // console.log(`users: ${users.length}`);
     try {
-      const tx = lucid.newTx()
-        .payToContract(
-          contract.address,
-          {
-            inline: Data.to(datum),
-            scriptRef: contract.validator,
-          },
-          { lovelace: 42n },
+      // for (const user of users) {
+      const hashes = await user
+        .generateActions()
+        .then((actions) =>
+          Promise.all(
+            actions.map(async (action) => {
+              const type = action.type;
+              actionCounts.set(type, (actionCounts.get(type) ?? 0) + 1);
+              const tx = action.tx(user.lucid.newTx());
+              // console.log(tx);
+              return await tx
+                .complete()
+                .then((completed) => {
+                  // console.log(completed.txComplete.to_js_value());
+                  return completed
+                    .sign()
+                    .complete()
+                    .then((signed) => {
+                      // console.log(signed.txSigned.to_js_value());
+                      return signed.submit();
+                    });
+                });
+            }),
+          )
         );
-      const txComplete = await tx.complete();
-      const signedTx = await txComplete.sign().complete();
-      await signedTx.submit();
-
-      emulator.awaitBlock(4);
-
-      const utxos = await lucid.utxosAt(contract.address);
-      console.log(utxos);
-
-      const tx2 = lucid.newTx()
-        // .attachMintingPolicy(contract.mintingPolicy)
-        // .mintAssets(burningNFTs, Lucid.Data.void()) // NOTE the Lucid.Data.void() redeemer is crucial
-        .addSigner(address)
-        .collectFrom(
-          utxos,
-          Lucid.Data.void(),
-        );
-      const tx2complete = await tx2.complete();
-      const signedTx2 = await tx2complete.sign().complete();
-      await signedTx2.submit();
-      emulator.awaitBlock(4);
+      // console.log(hashes);
+      traces.push(...hashes);
+      // }
     } catch (e) {
-      throw new Error(e);
+      throw new Error(`Error: ${e}`);
     }
+    emulator.awaitBlock(Number(genPositive()));
   }
+  console.log(`traces.length: ${traces.length}`);
+  for (const [type, count] of actionCounts) {
+    console.log(`${type}: ${count}`);
+  }
+  // for (const user of allUsers) {
+  //   const lucid = await Lucid.Lucid.new(emulator);
+  //   const user_ = await User.from(lucid, user.privateKey);
+  //   console.log(await user_.lucid.wallet.getUtxos());
+  // }
 });
+
+// Deno.test("constr", async () => {
+//   for (let i = 0; i < 100; i++) {
+//     const privateKey = Lucid.generatePrivateKey();
+
+//     const address = await (await Lucid.Lucid.new(undefined, "Custom"))
+//       .selectWalletFromPrivateKey(privateKey).wallet.address();
+
+//     const emulator = new Lucid.Emulator([{
+//       address,
+//       assets: { lovelace: 3000000000n },
+//     }]);
+
+//     const lucid = await Lucid.Lucid.new(emulator);
+
+//     lucid.selectWalletFromPrivateKey(privateKey);
+
+//     const contract = new Contract(lucid);
+//     const param = PParam.ptype.genData();
+//     const param_ = new Param(
+//       KeyHash.fromCredential(
+//         lucid.utils.getAddressDetails(address).paymentCredential!,
+//       ),
+//       param.virtual,
+//       param.weights,
+//       param.jumpSizes,
+//     );
+//     const paramDatum = new ParamDatum(param_);
+//     const peuclidDatum = PPreEuclidDatum.genPType(); //only need this for ParamDatum, so this is fine
+
+//     const datum = peuclidDatum.pconstant(paramDatum);
+
+//     console.log(datum);
+//     (datum.fields[0] as Lucid.Constr<Data>).fields.forEach((f) => {
+//       console.log(f);
+//       console.log("---");
+//     });
+//     try {
+//       const tx = lucid.newTx()
+//         .payToContract(
+//           contract.address,
+//           {
+//             inline: Data.to(datum),
+//             scriptRef: contract.validator,
+//           },
+//           { lovelace: 42n },
+//         );
+//       const txComplete = await tx.complete();
+//       const signedTx = await txComplete.sign().complete();
+//       await signedTx.submit();
+
+//       emulator.awaitBlock(4);
+
+//       const utxos = await lucid.utxosAt(contract.address);
+//       console.log(utxos);
+
+//       const tx2 = lucid.newTx()
+//         // .attachMintingPolicy(contract.mintingPolicy)
+//         // .mintAssets(burningNFTs, Lucid.Data.void()) // NOTE the Lucid.Data.void() redeemer is crucial
+//         .addSigner(address)
+//         .collectFrom(
+//           utxos,
+//           Lucid.Data.void(),
+//         );
+//       const tx2complete = await tx2.complete();
+//       const signedTx2 = await tx2complete.sign().complete();
+//       await signedTx2.submit();
+//       emulator.awaitBlock(4);
+//     } catch (e) {
+//       throw new Error(e);
+//     }
+//   }
+// });
 
 // Deno.test("lucid-example", async () => {
 //   const l = 32n; // empirical maximum = 32n
