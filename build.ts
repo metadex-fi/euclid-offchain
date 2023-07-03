11e6e89015415088a151f2d6e1b82312cd43ec5e
Chris M. Hiatt <chris.hiatt@protonmail.com>
2023-07-03 15:14:56 +0200

    fiddling with build.ts

--- build.ts
+++ build.ts
@@ -30,23 +30,8 @@ await dnt.build({
 // Deno.copyFileSync("LICENSE", "dist/LICENSE");
 Deno.copyFileSync("README.md", "dist/README.md");
 
-/** Add necessary global import statements to NPM ES Module. */
-const coreFile = `const isNode = globalThis?.process?.versions?.node;
-if (isNode) {
-    if (typeof btoa === 'undefined') {globalThis.btoa = function (str) {return Buffer.from(str, 'binary').toString('base64');}; globalThis.atob = function (b64Encoded) {return Buffer.from(b64Encoded, 'base64').toString('binary');};}
-    const fetch = /* #__PURE__ */ await import(/* webpackIgnore: true */ "node-fetch");
-    const { Crypto } = /* #__PURE__ */ await import(/* webpackIgnore: true */ "@peculiar/webcrypto");
-    const { WebSocket } = /* #__PURE__ */ await import(/* webpackIgnore: true */ "ws");
-    const fs = /* #__PURE__ */ await import(/* webpackIgnore: true */ "fs");
-    if (!globalThis.WebSocket) globalThis.WebSocket = WebSocket;
-    if (!globalThis.crypto) globalThis.crypto = new Crypto();
-    if (!globalThis.fetch) globalThis.fetch = fetch.default;
-    if (!globalThis.Headers) globalThis.Headers = fetch.Headers;
-    if (!globalThis.Request) globalThis.Request = fetch.Request;
-    if (!globalThis.Response) globalThis.Response = fetch.Response;
-    if (!globalThis.fs) globalThis.fs = fs; 
-}
+// Optional: Additional build steps specific to Denodex
 
-${Deno.readTextFileSync("./dist/esm/src/core/core.js")}
-`;
+/** Add necessary global import statements to NPM ES Module. */
+const coreFile = `${Deno.readTextFileSync("./dist/esm/src/core/core.js")}`;
 Deno.writeTextFileSync("./dist/esm/src/core/core.js", coreFile);
