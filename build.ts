1d10bbe5df461697977060e0ddbe592e8ee23f97
Chris M. Hiatt <chris.hiatt@protonmail.com>
2023-07-03 15:08:36 +0200

    fiddling with build.ts

--- build.ts
+++ build.ts
@@ -1,6 +1,11 @@
+import { exec } from "https://deno.land/x/exec/mod.ts";
 import * as dnt from "https://deno.land/x/dnt@0.30.0/mod.ts";
-import * as esbuild from "https://deno.land/x/esbuild@v0.17.11/mod.js";
+import { dirname } from "https://deno.land/std/path/mod.ts";
 import packageInfo from "./package.json" assert { type: "json" };
+
+await exec("deno", ["run", "--unstable", "--allow-run", "build.ts"], {
+  cwd: "../lucid",
+});
 
 await dnt.emptyDir("./dist");
 
@@ -30,17 +35,6 @@ await dnt.build({
 
 // Deno.copyFileSync("LICENSE", "dist/LICENSE");
 Deno.copyFileSync("README.md", "dist/README.md");
-
-// Copy WebAssembly
-
-Deno.copyFileSync(
-  "../lucid/src/core/libs/cardano_multiplatform_lib/cardano_multiplatform_lib_bg.wasm",
-  "dist/esm/lucid/src/core/libs/cardano_multiplatform_lib/cardano_multiplatform_lib_bg.wasm",
-);
-Deno.copyFileSync(
-  "../lucid/src/core/libs/cardano_message_signing/cardano_message_signing_bg.wasm",
-  "dist/esm/lucid/src/core/libs/cardano_message_signing/cardano_message_signing_bg.wasm",
-);
 
 // //** Web ES Module */
 
@@ -82,26 +76,26 @@ await esbuild.build({
 });
 esbuild.stop();
 
-// /** Add necessary global import statements to NPM ES Module. */
-// const coreFile = `const isNode = globalThis?.process?.versions?.node;
-// if (isNode) {
-//     if (typeof btoa === 'undefined') {globalThis.btoa = function (str) {return Buffer.from(str, 'binary').toString('base64');}; globalThis.atob = function (b64Encoded) {return Buffer.from(b64Encoded, 'base64').toString('binary');};}
-//     const { createRequire } = /* #__PURE__ */ await import(/* webpackIgnore: true */ "module");
-//     const require = createRequire(import.meta.url);
-//     const fetch = /* #__PURE__ */ await import(/* webpackIgnore: true */ "node-fetch");
-//     const { Crypto } = /* #__PURE__ */ await import(/* webpackIgnore: true */ "@peculiar/webcrypto");
-//     const { WebSocket } = /* #__PURE__ */ await import(/* webpackIgnore: true */ "ws");
-//     const fs = /* #__PURE__ */ await import(/* webpackIgnore: true */ "fs");
-//     if (!globalThis.WebSocket) globalThis.WebSocket = WebSocket;
-//     if (!globalThis.crypto) globalThis.crypto = new Crypto();
-//     if (!globalThis.fetch) globalThis.fetch = fetch.default;
-//     if (!globalThis.Headers) globalThis.Headers = fetch.Headers;
-//     if (!globalThis.Request) globalThis.Request = fetch.Request;
-//     if (!globalThis.Response) globalThis.Response = fetch.Response;
-//     if (!globalThis.require) globalThis.require = require;
-//     if (!globalThis.fs) globalThis.fs = fs;
-// }
+/** Add necessary global import statements to NPM ES Module. */
+const coreFile = `const isNode = globalThis?.process?.versions?.node;
+if (isNode) {
+    if (typeof btoa === 'undefined') {globalThis.btoa = function (str) {return Buffer.from(str, 'binary').toString('base64');}; globalThis.atob = function (b64Encoded) {return Buffer.from(b64Encoded, 'base64').toString('binary');};}
+    const { createRequire } = /* #__PURE__ */ await import(/* webpackIgnore: true */ "module");
+    const require = createRequire(import.meta.url);
+    const fetch = /* #__PURE__ */ await import(/* webpackIgnore: true */ "node-fetch");
+    const { Crypto } = /* #__PURE__ */ await import(/* webpackIgnore: true */ "@peculiar/webcrypto");
+    const { WebSocket } = /* #__PURE__ */ await import(/* webpackIgnore: true */ "ws");
+    const fs = /* #__PURE__ */ await import(/* webpackIgnore: true */ "fs");
+    if (!globalThis.WebSocket) globalThis.WebSocket = WebSocket;
+    if (!globalThis.crypto) globalThis.crypto = new Crypto();
+    if (!globalThis.fetch) globalThis.fetch = fetch.default;
+    if (!globalThis.Headers) globalThis.Headers = fetch.Headers;
+    if (!globalThis.Request) globalThis.Request = fetch.Request;
+    if (!globalThis.Response) globalThis.Response = fetch.Response;
+    if (!globalThis.require) globalThis.require = require;
+    if (!globalThis.fs) globalThis.fs = fs;
+}
 
-// ${Deno.readTextFileSync("./dist/esm/src/core/core.js")}
-// `;
-// Deno.writeTextFileSync("./dist/esm/src/core/core.js", coreFile);
+${Deno.readTextFileSync("./dist/esm/src/core/core.js")}
+`;
+Deno.writeTextFileSync("./dist/esm/src/core/core.js", coreFile);
