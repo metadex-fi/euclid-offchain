b511b699e390cd0fc8749711f76f94c61982ca1f
Chris M. Hiatt <chris.hiatt@protonmail.com>
2023-08-03 17:00:43 +0100

    conversion

--- src/utils/conversion.ts
+++ src/utils/conversion.ts
@@ -1,7 +1,7 @@
 // import { TransactionUnspentOutput } from "../../lucid.mod.ts";
-import { Address, AssetName, BigNum, MultiAsset, ScriptHash, TransactionHash, TransactionInput, TransactionOutput, TransactionUnspentOutput, Value } from "https://deno.land/x/lucid@0.10.6/src/core/libs/cardano_multiplatform_lib/cardano_multiplatform_lib.generated.js";
+import { Address, AssetName, BigNum, MultiAsset, ScriptHash, TransactionHash, TransactionInput, TransactionOutput, TransactionUnspentOutput, TransactionUnspentOutputs, Value } from "https://deno.land/x/lucid@0.10.6/src/core/libs/cardano_multiplatform_lib/cardano_multiplatform_lib.generated.js";
 import { Lucid } from "../../lucid.mod.ts";
-import { Asset } from "../mod.ts";
+import { Asset, Currency } from "../mod.ts";
 
 // TODO proptests
 
@@ -12,20 +12,45 @@ export const utxoToCML = (utxo: Lucid.UTxO): TransactionUnspentOutput => {
     BigNum.from_str(utxo.outputIndex.toString()),
   );
   const assets = MultiAsset.new();
+  const value = Value.zero();
   Object.entries(utxo.assets).forEach(([id, amount]) => {
     const asset = Asset.fromLucid(id);
-    const scriptHash = ScriptHash.from_hex(asset.currency.toLucid()); // TODO check those conversions are correct
-    const assetName = AssetName.new(Lucid.fromHex(asset.token.toLucid()));
-    const amount_ = BigNum.from_str(amount.toString());
-    assets.set_asset(
-      scriptHash,
-      assetName,
-      amount_,
-    );
+    if (asset.currency.equals(Currency.ADA)) {
+      value.set_coin(BigNum.from_str(amount.toString()));
+    } else {
+      const scriptHash = ScriptHash.from_hex(asset.currency.toLucid()); // TODO check those conversions are correct
+      const assetName = AssetName.new(Lucid.fromHex(asset.token.toLucid()));
+      const amount_ = BigNum.from_str(amount.toString());
+      assets.set_asset(
+        scriptHash,
+        assetName,
+        amount_,
+      );
+    }
   });
+  value.set_multiasset(assets);
   const txout = TransactionOutput.new(
-    Address.from_bytes(Lucid.fromHex(utxo.address)), // TODO correct?
-    Value.new_from_assets(assets)
+    Address.from_bech32(utxo.address), // TODO correct?
+    value,
   );
   return TransactionUnspentOutput.new(txin, txout);
 };
+
+export const utxosToCML = (
+  utxos: Lucid.UTxO[], 
+  mempoolUtxos: {
+    txHash: string;
+    outputIndex: number;
+  }[] 
+  ): TransactionUnspentOutputs => {
+    const utxos_ = TransactionUnspentOutputs.new();
+    utxos.forEach((utxo) => {
+      const i = mempoolUtxos.findIndex((mUtxo) => 
+      mUtxo.txHash === utxo.txHash && mUtxo.outputIndex === utxo.outputIndex
+      );
+      if (i === -1) {
+        utxos_.add(utxoToCML(utxo));
+      }
+    });
+    return utxos_;
+}
