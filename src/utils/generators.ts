665c840b7d319b6f19e6b406e2806271ea81dab0
Chris M. Hiatt <chris.hiatt@protonmail.com>
2023-09-09 14:30:04 +0100

    big numbers

--- src/utils/generators.ts
+++ src/utils/generators.ts
@@ -4,10 +4,10 @@ import { assert } from "https://deno.land/std@0.167.0/testing/asserts.ts";
 import { Lucid } from "../../lucid.mod.ts";
 import { PData } from "../types/general/fundamental/type.ts";
 
-// export const maxInteger = BigInt( // TODO better value, maybe look at chain/plutus max
-//   Number.MAX_SAFE_INTEGER,
-// ); // TODO handle those big numbers in contract code (fails onchain sometimes)
-export const maxInteger = 9000n; // NOTE This will cause weirdness after we added the minAda of 1000000n (utxo.ts)
+export const maxInteger = BigInt( // TODO better value, maybe look at chain/plutus max
+  Number.MAX_SAFE_INTEGER,
+); // TODO handle those big numbers in contract code (fails onchain sometimes)
+// export const maxInteger = 9000n; // NOTE This will cause weirdness after we added the minAda of 1000000n (utxo.ts)
 export const gMaxStringLength = 9000n; //maxInteger;
 export const gMaxStringBytes = gMaxStringLength / 2n;
 export const gMaxLength = 3n;
